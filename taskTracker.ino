8da793a3987d7a7ce500ed53fba98edd106fab96
Kiera Acitelli <kacitelli@gmail.com>
2024-11-01 10:18:27 -0400

    Add reset light time interval and button press logic

--- taskTracker.ino
+++ taskTracker.ino
@@ -1,36 +1,36 @@
 #define BTN_PIN 6
 #define LIGHT_PIN 4
-#define RESET_INTERVAL_SECONDS 15
+#define RESET_INTERVAL_SECONDS 10
+
+int LIGHT_STATE;
+unsigned long previousMillis = 0;
+unsigned long interval = (1000 * RESET_INTERVAL_SECONDS);
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(BTN_PIN, INPUT_PULLUP);
   pinMode(LIGHT_PIN, OUTPUT);
   Serial.begin(9600);
+  LIGHT_STATE=0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //TODO:
-  //Logic - Callibration with hold down button
-  //Logic - Once pressed, light on until callibration interval passes
-  //Electrical - Wire up speaker
-  //Logic - Button press triggers speaker audio of light state
-  //Electrical - wireless transmission from button
-  //Logic - receive wireless transmission from button
-  
-
+  //TODO: Callibration button, light on until callibration interval passes
   uint8_t state = digitalRead(BTN_PIN);
 
-  if (state == HIGH)
+  //When button is pressed, light state 1, turn on light
+  if (state == LOW)
   {
-    Serial.println("Activated!");
-    digitalWrite(LIGHT_PIN,  LOW);
-  }
-  else if (state == LOW)
-  {
-    Serial.println("Not activated.");
+    //Serial.println("Activated!");
     digitalWrite(LIGHT_PIN,  HIGH);
+    LIGHT_STATE = 1;
+  }
+
+  //if timer > reset interval, light state 0, turn off light, reset timer
+  if (millis() - previousMillis > interval){
+    //Serial.println("Resetting!");
+    previousMillis += interval;
+    digitalWrite(LIGHT_PIN,  LOW);
+    LIGHT_STATE = 0;
   }
   delay(200);
 }
