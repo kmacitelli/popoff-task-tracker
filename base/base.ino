799304e20f84d1c6fb2cb5d10904fceefd542b89
Kiera <kacitelli@gmail.com>
2025-01-18 11:47:22 -0500

    Add counter read logic and analog input

--- base/base.ino
+++ base/base.ino
@@ -13,10 +13,23 @@
 
 #define RESET_INTERVAL_SECONDS 10
 #define RECEIVER_PIN 13
+#define ID_PIN A0
+
+#define COUNTER_0 73
+#define COUNTER_1 244
+#define COUNTER_2 65
+#define COUNTER_3 396
+#define COUNTER_4 58
+#define COUNTER_5 95
+#define COUNTER_6 113
+#define COUNTER_7 137
+#define COUNTER_8 82
+#define COUNTER_9 176
 
 int LIGHT_STATE;
 unsigned long previousMillis = 0;
 unsigned long interval = (1000 * RESET_INTERVAL_SECONDS);
+int sensorValue = 0;
 
 void setup() {
   pinMode(RECEIVER_PIN, INPUT_PULLUP);
@@ -33,6 +46,10 @@ void loop() {
   //TODO: Callibration button, light on until callibration interval passes
   uint8_t completionState = digitalRead(RECEIVER_PIN);
   uint8_t buttonState = digitalRead(BUTTON_PIN);
+  sensorValue = analogRead(ID_PIN);
+
+
+  Serial.println(readCounterVoltageAsDigit(sensorValue));
 
   //When button is pressed, light state 1, turn on light
   if (completionState == LOW)
@@ -71,3 +88,36 @@ void turnOffLights(){
   digitalWrite(LIGHT_PIN_2, LOW);
   digitalWrite(LIGHT_PIN_3, LOW);
 }
+
+int avg(int num1, int num2){
+  return (num1+num2)/2;
+}
+
+int readCounterVoltageAsDigit(int counterVolt){
+  int digitRepresented;
+
+  if (counterVolt<avg(COUNTER_4, COUNTER_2)){
+    digitRepresented=4;
+  } else if (counterVolt<avg(COUNTER_2, COUNTER_0)){
+    digitRepresented=2;
+  } else if (counterVolt<avg(COUNTER_0, COUNTER_8)){
+    digitRepresented=0;
+  } else if (counterVolt<avg(COUNTER_8, COUNTER_5)){
+    digitRepresented=8;
+  } else if (counterVolt<avg(COUNTER_5, COUNTER_6)){
+    digitRepresented=5;
+  } else if (counterVolt<avg(COUNTER_6, COUNTER_7)){
+    digitRepresented=6;
+  } else if (counterVolt<avg(COUNTER_7, COUNTER_9)){
+    digitRepresented=7;
+  } else if (counterVolt<avg(COUNTER_9, COUNTER_1)){
+    digitRepresented=9;
+  } else if (counterVolt<avg(COUNTER_1, COUNTER_3)){
+    digitRepresented=1;
+  } else {
+    digitRepresented=3;
+  } 
+
+  return digitRepresented;
+}
+
