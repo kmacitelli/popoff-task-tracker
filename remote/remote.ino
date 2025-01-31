42386a0590cf4b636cb22a27e738d8319bd4ed1e
unknown <kacitelli@gmail.com>
2025-01-30 20:30:43 -0500

    Update pin numbers and add light and beep remote logic

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,17 +1,19 @@
+//Remote IO
+#define LIGHT_PIN 5
+#define BUZZER_PIN 3
+#define BUTTON_PIN 1
+#define ID_PIN A6
+
+//Remote radio stuff
 #define RADIO_RST_PIN 2
 #define RADIO_IRQ_PIN 3
 #define RADIO_CS_PIN 4
-#define LIGHT_PIN 5
-#define BUZZER_PIN 8
-#define BUTTON_PIN 9
-
 #define RADIO_MOSI_PIN 11
 #define RADIO_MISO_PIN 12
 #define RADIO_SCLK_PIN 13
-
 #define TRANSMITTER_PIN 13
-#define ID_PIN A6
 
+//Thresholds for analog input for counter numbers
 #define COUNTER_0 388
 #define COUNTER_1 193
 #define COUNTER_2 222
@@ -23,7 +25,8 @@
 #define COUNTER_8 171
 #define COUNTER_9 515
 
-int sensorValue;
+//Unique ID of the module for use pairing to base
+int counterUID;
 
 void setup() {
   //pinMode(TRANSMITTER_PIN, OUTPUT_PULLUP);
@@ -32,32 +35,41 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
 
   Serial.begin(9600);
+
+  counterUID = readCounterVoltageAsDigit(analogRead(ID_PIN));
 }
 
 void loop() {
-  //uint8_t buttonState = digitalRead(BUTTON_PIN);
-  sensorValue = readCounterVoltageAsDigit(analogRead(ID_PIN));
-  Serial.println(sensorValue);
-
-  //if (buttonState == HIGH){
-  //  Serial.println("is pressed");
-  //  buttonPressed();
-  //}
-  //else{
-  //  Serial.println("Not pressed");
-  //}
-  delay(500);
-}
-void buttonPressed(){
-  //When button is pressed, blink light, beep, and signal
-  //turn on light
-  //beep noise
-  //delay
-  //send signal to base
-  //turn off light and noise
+  //Read from button
+  uint8_t buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == HIGH){
+    //Serial.println("not pressed");
+  }
+  else{
+    //Serial.println("is pressed");
+    buttonPressed();
+  }
   delay(100);
 }
+void buttonPressed(){
+  //if button is pressed, we need to send signal to base, beep, and light up
+  digitalWrite(LIGHT_PIN, HIGH);
+  digitalWrite(BUZZER_PIN, HIGH);
+  sendButtonSignal();
 
+  //Noise for shorter time than light
+  delay(200);
+  digitalWrite(BUZZER_PIN, LOW);
+  delay(1400);
+  digitalWrite(LIGHT_PIN, LOW);
+}
+
+//use our radio module to send a signal to base including our UID
+void sendButtonSignal(){
+  //todo radio here
+}
+
+//Given analog input from counter, determine digit based on the reading
 int readCounterVoltageAsDigit(int counterVolt){
   int digitRepresented;
 
@@ -86,6 +98,7 @@ int readCounterVoltageAsDigit(int counterVolt){
   return digitRepresented;
 }
 
+//quick and dirty way to make ranges for counter input values
 int avg(int num1, int num2){
   return (num1+num2)/2;
 }
